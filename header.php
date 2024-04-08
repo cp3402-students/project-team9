b383aea7b5dc216e6e73460a9a5161c4c5355867
BeauWilliams97 <beau.williams@my.jcu.edu.au>
2024-04-08 22:33:47 +1000

    -replaced all bootstrap directory paths with bootstrap links

--- header.php
+++ header.php
@@ -19,7 +19,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <?php wp_head(); ?>
     <!-- include bootstrap css -->
-    <link href="./node_modules/bootstrap/dist/js/bootstrap.bundle.min.js" rel="stylesheet">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
 </head>
 
 <body <?php body_class(); ?>>
@@ -112,3 +112,4 @@
                 </div>
             </div>
         </header>
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
