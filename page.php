023b516059e7b39f9aacef16d3d149ac6924bc00
BeauWilliams97 <beau.williams@my.jcu.edu.au>
2024-04-11 15:15:14 +1000

    -various styling changes -added outer content div to page.php in order to change colour div surrounding page content -fixed offcanvas navbar menu item styling

--- page.php
+++ page.php
@@ -16,33 +16,25 @@ get_header();
 ?>
 
 <main id="primary" class="site-main">
-    <div class="custom-jumbotron">
-        <?php
-        // Output the jumbotron
-        display_custom_jumbotron();
-        ?>
-    </div>
+    <div class="outer-content-container">
+        <div class="content-container">
+            <?php
+            // Always display the outer content container
+            // No matter if there's content or not
+            while (have_posts()): the_post();
 
-    <div class="content-container">
-        <?php
-        while (have_posts()):
-            the_post();
+                // Output the post/page content
+                the_content();
 
-            // Start of the content container
-            echo '<div class="content-container">';
-            // Output the post/page content
-            the_content();
-            // End of the content container
-            echo '</div>';
+                // If comments are open or we have at least one comment, load up the comment template.
+                if (comments_open() || get_comments_number()):
+                    comments_template();
+                endif;
 
-            // If comments are open or we have at least one comment, load up the comment template.
-            if (comments_open() || get_comments_number()):
-                comments_template();
-            endif;
-
-        endwhile; // End of the loop.
-        ?>
-    </div><!-- .content-container -->
+            endwhile;
+            ?>
+        </div><!-- .content-container -->
+    </div><!-- .outer-content-container -->
 </main><!-- #primary -->
 
 <?php
